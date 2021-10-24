988e375531a50fd829fe0755ee8d83a8c07a6dec
rodrigonahid <rodrigonahid1@gmail.com>
2021-10-24 19:42:12 -0300

    finished ex 5

---
+++ basic/exercise-05/index.js
@@ -0,0 +1,21 @@
+"use strict";
+const Five = {
+    element: document.querySelector('#app'),
+    text: 'w3resources',
+    rotate: () => {
+        let RotatedObj = Five.text.split('');
+        let NewElement = document.createElement('p');
+        const TextLength = Five.text.length - 1;
+        NewElement.innerHTML = RotatedObj.join('');
+        Five.element.appendChild(NewElement);
+        RotatedObj.forEach((item, index) => {
+            RotatedObj.unshift(RotatedObj[TextLength]);
+            RotatedObj.pop();
+            let NewElement = document.createElement('p');
+            NewElement.innerHTML = RotatedObj.join('');
+            console.log(RotatedObj.join(''), NewElement);
+            Five.element.appendChild(NewElement);
+        });
+    },
+};
+Five.rotate();
